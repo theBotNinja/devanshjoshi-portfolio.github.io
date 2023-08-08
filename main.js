24cdf2cf2119441681a2e0d5c9c032f7e9b7b038
devansh joshi <devansh0814dj@gmail.com>
2023-08-09 01:58:54 +0530

    mediaQ added

--- main.js
+++ main.js
@@ -24,45 +24,24 @@ const observerAtInfo = new IntersectionObserver((entries)=>{
         }
     });
 });
+const observerAtSection5 = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if (entry.isIntersecting){
+            let title = document.getElementById("titleForProject");
+            title.textContent = "Projects";
+            title.style.color = "white";
+        }else{
+            let title = document.getElementById("titleForProject");
+            title.style.color = "#c6b600";
+            title.textContent = "Contacts";
+        }
+    });
+});
 let sections = document.querySelectorAll(".commonInSection");
 sections.forEach((el)=>observerAtSection2.observe(el));
 
 let info = document.querySelectorAll(".info2");
 info.forEach((el2)=>observerAtInfo.observe(el2));
 
-
-/*
-Sure, here is an example of how Devansh Joshi could introduce himself on his portfolio website:
-
-Hi, I'm Devansh Joshi, a third-year B.Tech. student in Computer Science and Engineering at MMMUT Gorakhpur.
-
-I'm passionate about technology and I'm always looking for new ways to use it to solve problems and create innovative solutions.
-
-In my spare time, I enjoy coding, playing video games, and spending time with my friends and family.
-
-I'm looking for an internship or full-time position where I can use my skills and creativity to make a difference in the world.
-
-Here are some of my skills and accomplishments:
-
-Proficient in Java, Python, and C++
-Experience with web development, mobile development, and data science
-Winner of the MMMUT ACM-ICPC Programming Contest
-Member of the MMMUT Robotics Team
-I'm a hard worker and I'm always eager to learn new things. I'm confident that I can make a valuable contribution to your team.
-
-Thank you for your time and consideration.
-
-Sincerely,
-Devansh Joshi
-
-This introduction is brief, but it gives a clear and concise overview of Devansh's skills, experience, and personality. It also includes a call to action, encouraging visitors to contact him if they are interested in his work.
-
-Here are some additional tips for writing a strong introduction for your portfolio website:
-
-Start with a strong hook that will grab the reader's attention.
-Be clear and concise about who you are and what you do.
-Highlight your skills and accomplishments.
-Add a personal touch by sharing something interesting about yourself.
-Use a friendly and professional tone.
-Proofread your introduction carefully before publishing it.
-I hope this helps!*/
+let project_section = document.querySelectorAll(".newSection5");
+project_section.forEach((el3)=>observerAtSection5.observe(el3));
