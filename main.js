1481ccb0392e739c44b5736147342740c1ffca2b
devansh joshi <devansh0814dj@gmail.com>
2023-08-10 21:27:04 +0530

    seems ready all links and minor bits added

--- main.js
+++ main.js
@@ -45,3 +45,18 @@ info.forEach((el2)=>observerAtInfo.observe(el2));
 
 let project_section = document.querySelectorAll(".newSection5");
 project_section.forEach((el3)=>observerAtSection5.observe(el3));
+
+function openupfor(){
+    let nth_list = document.getElementById("Section6").children
+    nth_list[0].style = "opacity : 0%"
+    nth_list[1].style = "transform: translateX(100%);"
+     nth_list[2].style = "transform: translateX(0%)"
+      nth_list[4].style = "transform: translateX(0%);"
+}
+function backhowitwas(){
+    let nth_list = document.getElementById("Section6").children
+    nth_list[0].style = "opacity : 50%"
+    nth_list[1].style = "transform: rotateZ(10deg);"
+     nth_list[2].style = "transform: rotateZ(-10deg);"
+      nth_list[4].style = "transform: rotateZ(5deg);"
+}
